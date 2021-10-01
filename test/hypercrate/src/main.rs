56a671b694fa729012b0ea7b54aae772683c93ce
clux <sszynrae@gmail.com>
2021-10-01 14:43:54 +0100

    bump hyper ecosystem in tests (was failing)
    Signed-off-by: clux <sszynrae@gmail.com>

--- test/hypercrate/src/main.rs
+++ test/hypercrate/src/main.rs
@@ -1,40 +1,26 @@
-extern crate futures;
-extern crate hyper;
-extern crate tokio_core;
-extern crate hyper_openssl;
+use hyper_tls::HttpsConnector;
+use hyper::{body::HttpBody as _, Client};
+use tokio::io::{self, AsyncWriteExt as _};
 
-use std::env;
-use std::io::{self, Write};
-
-use futures::Future;
-use futures::stream::Stream;
-
-use hyper::Client;
-use hyper_openssl::HttpsConnector;
-
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     // set SSL_CERT location - see issue #5
-    // normally you'd want to set this in your docker container
+    // normally you'd want to set this in your container
     // but for plain bin distribution and this test, we set it here
-    env::set_var("SSL_CERT_FILE", "/etc/ssl/certs/ca-certificates.crt");
+    std::env::set_var("SSL_CERT_FILE", "/etc/ssl/certs/ca-certificates.crt");
 
     let url = "https://raw.githubusercontent.com/clux/muslrust/master/README.md";
-    let url = url.parse::<hyper::Uri>().unwrap();
 
-    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
-    let client = Client::configure()
-        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
-        .build(&core.handle());
+    let mut res = client.get(url.parse()?).await?;
+    assert_eq!(res.status(), 200);
 
-    let work = client.get(url).and_then(|res| {
-        println!("Response: {}", res.status());
-        assert!(res.status().is_success());
+    while let Some(next) = res.data().await {
+        let chunk = next?;
+        io::stdout().write_all(&chunk).await?;
+    }
 
-        res.body().for_each(|chunk| {
-            io::stdout().write_all(&chunk).map_err(From::from)
-        })
-    });
-
-    core.run(work).unwrap();
+    Ok(())
 }
