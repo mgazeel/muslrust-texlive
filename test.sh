1af7be74a01bd61c534899abd2ce1e97304251ef
Eran Rundstein <eran@rundste.in>
2024-02-23 20:50:20 -0800

    Switch the nightly build to producing a multiarch docker image that supports both x86_64 and arm64

--- test.sh
+++ test.sh
@@ -2,18 +2,31 @@
 set -ex
 
 docker_build() {
+  echo "Target dir: $TARGET_DIR"
+  echo "Platform: $PLATFORM"
+
   # NB: add -vv to cargo build when debugging
   local -r crate="$1"crate
   docker run --rm \
     -v "$PWD/test/${crate}:/volume" \
     -v cargo-cache:/root/.cargo/registry \
     -e RUST_BACKTRACE=1 \
-    clux/muslrust:temp \
+    -e AR=ar \
+    --platform $PLATFORM \
+    rustmusl-temp \
     cargo build
+
   cd "test/${crate}"
-  ./target/x86_64-unknown-linux-musl/debug/"${crate}"
-  ldd "target/x86_64-unknown-linux-musl/debug/${crate}" 2>&1 | grep -qE "not a dynamic|statically linked" && \
-    echo "${crate} is a static executable"
+
+  # Ideally we would use `ldd` but due to a qemu bug we can't :(
+  # See https://github.com/multiarch/qemu-user-static/issues/172
+  # Instead we use `file`.
+  docker run --rm \
+    -v "$PWD:/volume" \
+    -e RUST_BACKTRACE=1 \
+    --platform $PLATFORM \
+    test-runner \
+    bash -c "cd volume; ./target/$TARGET_DIR/debug/${crate} && file ./target/$TARGET_DIR/debug/${crate} && file /volume/target/$TARGET_DIR/debug/${crate} 2>&1 | grep -qE 'static-pie linked|statically linked' && echo ${crate} is a static executable"
 }
 
 # Helper to check how ekidd/rust-musl-builder does it
